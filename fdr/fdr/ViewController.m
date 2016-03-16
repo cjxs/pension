9453798d16e8628e466b6ac2e58e4e99ec38cb35
cd <13083722838@163.com>
2016-03-16 15:12:33 +0800

    我又改了

--- fdr/fdr/ViewController.m
+++ fdr/fdr/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <UIKit/UIKit.h>
 @interface ViewController ()
 
 @end
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel * label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
